fb44f3e55697aed0d53ecab03bc67eea9ed7241b
William Jackson <william@subtlecoolness.com>
2021-05-26 09:15:39 -0500

    Update add-origin-to-title.user.js

--- add-origin-to-title.user.js
+++ add-origin-to-title.user.js
@@ -1,19 +1,21 @@
 // ==UserScript==
-// @name         Add origin to title
-// @version      0.8.1
-// @description  Add origin to title
-// @author       William Jackson
-// @match        http://*/*
-// @match        https://*/*
+// @name        Add origin to title
+// @namespace   https://github.com/williamjacksn/userscripts
+// @match       http*://*/*
+// @version     2021.1
+// @description Add origin to title
+// @run-at      document-idle
+// @grant       none
+// @author      William Jackson
 // ==/UserScript==
 
 function addOriginToTitle() {
-    "use strict";
-    let tag = "[ " + location.origin + " ]";
+    'use strict';
+    let tag = `[ ${location.origin} ]`;
     if (document.title.includes(tag)) {
         // pass
     } else {
-        document.title = document.title + " " + tag;
+        document.title = `${document.title} ${tag}`;
     }
 }
 
