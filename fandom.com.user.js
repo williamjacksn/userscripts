72968f29a5ff74dbe778f8d6b136198501745921
William Jackson <565174+williamjacksn@users.noreply.github.com>
2022-05-04 10:51:29 -0500

    Update fandom.com.user.js

--- fandom.com.user.js
+++ fandom.com.user.js
@@ -2,7 +2,7 @@
 // @name        Adjustments for fandom.com
 // @namespace   https://github.com/williamjacksn/userscripts
 // @match       https://*.fandom.com/*
-// @version     2022.2
+// @version     2022.3
 // @description Adjustments for fandom.com
 // @run-at      document-idle
 // @grant       none
@@ -12,6 +12,7 @@
 function adjustPage() {
     'use strict';
     const selectorsToRemove = [
+        'div.ad-slot',
         'div.featured-video__wrapper',
         'div.top-ads-container',
         'div#WikiaBar',
