bd16951b3e0e880699e076a185e993c1dc674c0f
William Jackson <565174+williamjacksn@users.noreply.github.com>
2022-05-04 10:16:25 -0500

    Create outlook.live.com.user.js

---
+++ outlook.live.com.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name        Adjustments for outlook.live.com
+// @namespace   https://github.com/williamjacksn/userscripts
+// @match       https://outlook.live.com/*
+// @version     2022.1
+// @description Adjustments for outlook.live.com
+// @run-at      document-idle
+// @grant       none
+// @author      William Jackson
+// ==/UserScript==
+
+function adjustPage() {
+    'use strict';
+    document.querySelectorAll('div#owaadbar0').forEach((el) => {
+        el.parentNode.parentNode.parentNode.remove();
+    });
+    document.querySelectorAll('div.VPtFloqixpkkqJNcOv2T'),forEach((el) => {
+        el.remove();
+    });
+}
+
+(function() {
+    adjustPage();
+    setInterval(adjustPAge, 10000);
+})();
