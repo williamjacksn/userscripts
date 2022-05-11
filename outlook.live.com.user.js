0dd73bc8e6821c2be0d96d8680c11a36a7d90dc3
William Jackson <565174+williamjacksn@users.noreply.github.com>
2022-05-11 15:28:53 -0500

    Update outlook.live.com.user.js

--- outlook.live.com.user.js
+++ outlook.live.com.user.js
@@ -2,7 +2,7 @@
 // @name        Adjustments for outlook.live.com
 // @namespace   https://github.com/williamjacksn/userscripts
 // @match       https://outlook.live.com/*
-// @version     2022.3
+// @version     2022.4
 // @description Adjustments for outlook.live.com
 // @run-at      document-idle
 // @grant       none
@@ -13,6 +13,9 @@ function adjustPage() {
     'use strict';
     document.querySelectorAll('div#owaadbar0').forEach((el) => {
         el.parentNode.parentNode.parentNode.remove();
+    });
+    document.querySelectorAll('div.VPtFl').forEach((el) => {
+        el.remove();
     });
     document.querySelectorAll('div.VPtFloqixpkkqJNcOv2T').forEach((el) => {
         el.remove();
