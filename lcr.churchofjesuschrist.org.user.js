aa3ccf01a3eda5d4b1487aed813a3dce85be5629
William Jackson <565174+williamjacksn@users.noreply.github.com>
2022-07-24 15:20:08 -0500

    Update lcr.churchofjesuschrist.org.user.js

--- lcr.churchofjesuschrist.org.user.js
+++ lcr.churchofjesuschrist.org.user.js
@@ -3,13 +3,20 @@
 // @namespace   https://github.com/williamjacksn/userscripts
 // @match       https://lcr.churchofjesuschrist.org/records/member-list
 // @grant       none
-// @version     2022.0
+// @version     2022.1
 // @author      William Jackson
 // @description Adjustments for lcr.churchofjesuschrist.org
 // ==/UserScript==
 
 
 function adjustPage() {
+    const t = document.querySelector('table.member-list');
+    if (t) {
+        console.log(`I found the table! It has ${t.rows.length} rows.`);
+    }
+}
+
+function logMembers() {
     'use strict';
     const memberRows = document.querySelector('table.member-list tbody tr');
     memberRows.forEach((row) => {
@@ -21,4 +28,5 @@ function adjustPage() {
 
 (function() {
     adjustPage();
+    setInterval(adjustPage, 10000);
 })();
