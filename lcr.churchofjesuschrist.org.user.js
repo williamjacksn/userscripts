e7d337aafb63a2af9d7e19fd634b3c92f3a39fc5
William Jackson <565174+williamjacksn@users.noreply.github.com>
2022-07-24 16:03:54 -0500

    Update lcr.churchofjesuschrist.org.user.js

--- lcr.churchofjesuschrist.org.user.js
+++ lcr.churchofjesuschrist.org.user.js
@@ -26,13 +26,25 @@ function adjustPage() {
 }
 
 function logMembers() {
-    'use strict';
+    let csvContent = 'data:text/csv;charset=utf-8,name,gender,age,birth_date,address,phone_number,email\n';
     const memberRows = document.querySelectorAll('table.member-list tbody tr');
     memberRows.forEach((row) => {
-        row.querySelectorAll('td').forEach((td) => {
-            console.log(`${td.innerText}`);
-        });
+        const dataFields = row.getElementsByTagName('td');
+        const memberName = JSON.stringify(dataFields[1].innerText);
+        const memberGender = JSON.stringify(dataFields[3].innerText);
+        const memberAge = JSON.stringify(dataFields[4].innerText);
+        const memberBirthDate = JSON.stringify(dataFields[5].innerText);
+        const memberAddress = JSON.stringify(dataFields[6].innerText);
+        const memberPhoneNumber = JSON.stringify(dataFields[7].innerText);
+        const memberEmail = JSON.stringify(dataFields[8].innerText);
+        csvContent += `${memberName},${memberGender},${memberAge},${memberBirthDate},${memberAddress},${memberPhoneNumber},${memberEmail}\n`;
     });
+    let encodedUri = encodeURI(csvContent);
+    let link = document.createElement('a');
+    link.setAttribute('href', encodedUri);
+    link.setAttribute('download', 'member-list.csv');
+    document.body.appendChild(link);
+    link.click();
 }
 
 (function() {
